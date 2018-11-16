a3c2eab294e6dc59a4c47857c5c124740ae71906
Jaan <jaan.tollander@gmail.com>
2018-11-16 11:09:02 +0200

    A5.1 finished

--- A5/A_5_1_one_euro_filter.py
+++ A5/A_5_1_one_euro_filter.py
@@ -1,8 +1,11 @@
 import math
+from os import makedirs
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas
+from matplotlib.animation import MovieWriter
+from matplotlib.cbook import mkdirs
 
 
 def low_pass_filter(a, x, x_prev):
@@ -45,13 +48,16 @@ class OneEuroFilter:
         return x_hat
 
 
-if __name__ == '__main__':
+def plots():
     df = pandas.read_csv("A_5_1_noise.csv")
     t, x = df.values[:, 0], df.values[:, 1]
 
     # Try different paramaters
-    for min_cutoff in [0.1, 0.05, 0.01, 0.005, 0.001]:
-        for beta in [0.05, 0.01, 0.007, 0.005, 0.003, 1e-3]:
+    directory = "A_5_1_figures"
+    makedirs(directory, exist_ok=True)
+    for min_cutoff in np.linspace(0.01, 0.005, 6):
+        for beta in np.linspace(0.01, 0.001, 10):
+            print("plotting:", min_cutoff, beta)
             one_euro_filter = OneEuroFilter(
                 t[0], x[0],
                 min_cutoff=min_cutoff, beta=beta)
@@ -60,8 +66,53 @@ if __name__ == '__main__':
             for i in range(1, len(t)):
                 x2[i] = one_euro_filter(t[i], x[i])
 
-            plt.plot(t, x, alpha=0.3, color="cyan")
+            plt.figure()
+            plt.plot(t, x, alpha=0.4, color="cyan")
             plt.plot(t, x2, color="blue")
-            plt.plot(t, np.abs(x-x2), color="red", alpha=0.3)
-            plt.title(f"min_cutoff: {min_cutoff}, beta: {beta}")
-            plt.show()
+            # plt.plot(t, np.abs(x-x2), color="red", alpha=0.3)
+            plt.title(f"min_cutoff: {min_cutoff:.3f}, beta: {beta:.3f}")
+            # plt.show()
+            plt.savefig(f"{directory}/{min_cutoff:.3f}_{beta:.3f}.png", dpi=300)
+
+
+def plot_signal():
+    import seaborn
+    seaborn.set()
+    df = pandas.read_csv("A_5_1_noise.csv")
+    t, x = df.values[:, 0], df.values[:, 1]
+
+    plt.figure()
+    plt.plot(t, x)
+    plt.xlabel("$t$")
+    plt.ylabel("$X$")
+    plt.savefig("figures/noise.png", dpi=300)
+
+
+def plot_filtered_signal(min_cutoff=0.008, beta=0.002):
+    import seaborn
+    seaborn.set()
+    df = pandas.read_csv("A_5_1_noise.csv")
+    t, x = df.values[:, 0], df.values[:, 1]
+
+    one_euro_filter = OneEuroFilter(
+        t[0], x[0],
+        min_cutoff=min_cutoff, beta=beta)
+    x2 = np.zeros_like(x)
+    x2[0] = x[0]
+    for i in range(1, len(t)):
+        x2[i] = one_euro_filter(t[i], x[i])
+
+    plt.figure()
+    plt.plot(t, x, alpha=0.4, color="cyan")
+    plt.plot(t, x2, color="blue")
+    plt.title(f"min_cutoff: {min_cutoff:.3f}, beta: {beta:.3f}")
+
+    plt.xlabel("$t$")
+    plt.ylabel("$X$")
+    plt.savefig(f"figures/noise_filtered_{min_cutoff:.3f}_{beta:.3f}.png", dpi=300)
+
+
+if __name__ == '__main__':
+    # plot_signal()
+    # plot_filtered_signal()
+    plots()
