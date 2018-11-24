3cc31330418b58e74fa8e9900a170e3f6856420d
Jaan <jaan.tollander@gmail.com>
2018-11-24 11:58:35 +0200

    A7 finished

--- A7/A_7_1_linear_menu.py
+++ A7/A_7_1_linear_menu.py
@@ -1,5 +1,4 @@
 from gurobipy import GRB, quicksum
-from math import log2
 
 import numpy as np
 from gurobi import Model
@@ -11,11 +10,8 @@ def dist(columns, i, j):
                    abs(i % columns - j % columns) ** 2)
 
 
-def solve(elements, positions, frequency, distance, rows, columns,
-          colocated=None):
+def solve(elements, positions, frequency, distance):
     # ==== 1. Create the (empty) model ====
-    if colocated is None:
-        colocated = []
     model = Model("linear_menu")
 
     # ==== 2. Add decision variables ======
@@ -39,32 +35,15 @@ def solve(elements, positions, frequency, distance, rows, columns,
     for e in elements:
         model.addConstr(quicksum(x[(e, p)] for p in positions) == 1,
                         "uniqueness_constraint_%s" % e)
-
-    # Add constraints to items that should be colocated.
-    if colocated is not None:
-        for (e1, e2) in colocated:
-            # 1. same row
-            # 2. same column
-            model.addConstr(
-                quicksum([x[(e1, i*columns+j)] * x[(e2, i*columns+(j+1))] +
-                          x[(e2, i*columns+j)] * x[(e1, i*columns+(j+1))] +
-                          x[(e1, i * columns + j)] * x[(e2, (i + 1) * columns + j)] +
-                          x[(e2, i * columns + j)] * x[(e1, (i + 1) * columns + j)]
-                          for i in range(rows-1)
-                          for j in range(columns-1)]) == 1,
-                f"Elements {e1}{e2} are colocated")
-
     model.update()
 
     # ==== 4. Specify Objective function ======
+    reading_cost = 0.4  # assumed that scanning a single item takes 400 ms
+
     # Sum up the costs for mapping any element e to any position p
-    a = 0
-    b = 1
-    width = 1
-    cost = quicksum(
-        (a + b * log2(distance[p] / width + 1)) * frequency[e] * x[(e, p)]
-        for e in elements
-        for p in positions)
+    cost = quicksum(frequency[e] * distance[p] * reading_cost * x[(e, p)]
+                    for e in elements
+                    for p in positions)
     model.setObjective(cost, GRB.MINIMIZE)
 
     # ==== 5. Optimize model ======
@@ -83,34 +62,37 @@ def solve(elements, positions, frequency, distance, rows, columns,
     return layout, model.getObjective().getValue()
 
 
-def random_frequecies(n):
-    s = np.random.sample(n)
-    return s / np.sum(s)
+def solve2(elements, positions, f1, w1, f2, w2, distance):
+    assert w1 > 0 and w2 > 0 and w1 + w2 == 1
+    frequency = dict()
+    for e in elements:
+        frequency[e] = w1 * f1[e] + w2 * f2[e]
+    return solve(elements, positions, frequency, distance)
 
-
-# Set random state
-np.random.seed(3)
 
 # define elements and positions
-n = 16
-columns = 4
-rows = n // columns
-
-elements = list(range(n))
+elements = ['Open', 'About', 'Quit', 'Help', 'Close',
+            'Save', 'Edit', 'Insert', 'Delete']
 positions = list(range(len(elements)))
 
-colocated = [(elements[1], elements[5])]
-
 # define cost factors
-frequency = {e: f for (e, f) in zip(elements, random_frequecies(n))}
-distance = list(map(lambda p: dist(columns, 0, p), positions))
+distance = list(map(lambda p: dist(1, 0, p), positions))
+
+f1 = {'Quit': 0.3, 'About': 0.2, 'Open': 0.1, 'Save': 0.1, 'Close': 0.05,
+      'Help': 0.02, 'Edit': 0.08, 'Insert': 0.1, 'Delete': 0.05}
+f2 = {'Quit': 0.02, 'About': 0.1, 'Open': 0.3, 'Save': 0.2, 'Close': 0.1,
+      'Help': 0.05, 'Edit': 0.05, 'Insert': 0.1, 'Delete': 0.08}
+w1 = 0.5
+w2 = 1 - w1
 
 # solve the problem
-layout, objective = solve(elements, positions, frequency, distance, rows,
-                          columns, colocated)
+layout1, objective1 = solve(elements, positions, f1, distance)
+layout2, objective2 = solve(elements, positions, f2, distance)
+layout, objective = solve2(elements, positions, f1, w1, f2, w2, distance)
 
 # Print the solution
-# TODO: mirror the answer along the axis
 print("Objective value (expected selection time):", objective)
-print(frequency)
-print(np.array(layout).reshape((columns, columns)))
+print()
+print("Layout 1:        ", layout1, "Objective value:", objective1)
+print("Layout 2:        ", layout2, "Objective value:", objective1)
+print("Combined Layout: ", layout, "Objective value:", objective)
